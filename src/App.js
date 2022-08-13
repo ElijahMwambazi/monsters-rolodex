d0e23b0be2272b601d7f6684152920432acb04f5
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-08-13 15:11:26 +0200

    Chore: Added search bar and implemented searching

--- src/App.js
+++ src/App.js
@@ -3,35 +3,54 @@ import './App.css';
 
 class App extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       monsters: [],
-    };
+      searchInput: ""
+    }
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users/')
-      .then((responce) => responce.json())
-      .then((users) => this.setState(
-        () => { return { monsters: users }; },
-        () => { console.log(this.state) }
-      ));
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => { return { monsters: users } },
+          () => { })
+      })
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchInput)
+    })
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          )
-        })}
+        <input
+          className="search-bar"
+          type="search"
+          placeholder="search monster(s)"
+          onChange={(event) => {
+            const searchInput = event.target.value.toLocaleLowerCase()
+
+            return this.setState({ searchInput })
+          }
+          }
+        />
+
+        {
+          filteredMonsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>)
+          })
+        }
       </div>
     )
   }
 }
 
-export default App;
+export default App
