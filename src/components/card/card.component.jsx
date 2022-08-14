1e5ab8ddcbdc32f472e3f9aea9583d8d0f2bf625
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-08-14 20:44:17 +0200

    Refactor: Converted components to functional

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,38 @@
 import { Component } from "react";
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const { name, email, id } = this.props.monster;
+// APP FUNCTIONAL COMPONENT
+const Card = ({ monster }) => {
+  const { name, email, id } = monster;
 
-    return (
-      <div key={id} className="card-container">
-        <img
-          src={`https://robohash.org/${id}/?set=set2&size=180x180`}
-          alt={`monster ${name}`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div key={id} className="card-container">
+      <img
+        src={`https://robohash.org/${id}/?set=set2&size=180x180`}
+        alt={`monster ${name}`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+// APP CLASS COMPONENT
+// class Card extends Component {
+//   render() {
+//     const { name, email, id } = this.props.monster;
+
+//     return (
+//       <div key={id} className="card-container">
+//         <img
+//           src={`https://robohash.org/${id}/?set=set2&size=180x180`}
+//           alt={`monster ${name}`}
+//         />
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default Card;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,19 +1,30 @@
-import { Component } from "react";
+// import { Component } from "react";
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-  render() {
-    const { props } = this;
+// APP FUNCTIONAL COMPONENT
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
 
-    return (
-      <input
-        className={`search-box ${props.className}`}
-        type="search"
-        placeholder={props.placeholder}
-        onChange={props.onChangeHandler}
-      />
-    );
-  }
-}
+// APP CLASS COMPONENT
+// class SearchBox extends Component {
+//   render() {
+//     const { props } = this;
+
+//     return (
+//       <input
+//         className={`search-box ${props.className}`}
+//         type="search"
+//         placeholder={props.placeholder}
+//         onChange={props.onChangeHandler}
+//       />
+//     );
+//   }
+// }
 
 export default SearchBox;
