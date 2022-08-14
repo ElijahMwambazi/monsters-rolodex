3dbf0feee8787e944ee450a74ab83e143930c033
Elijah Mwambazi <eijahmwambaxi@gmail.com>
2022-08-14 09:56:51 +0200

    Chore: Added style

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import "./search-box.styles.css";
 
 class SearchBox extends Component {
   render() {
@@ -6,6 +7,7 @@ class SearchBox extends Component {
 
     return (
       <input
+        className={`search-box ${props.className}`}
         type="search"
         placeholder={props.placeholder}
         onChange={props.onChangeHandler}
